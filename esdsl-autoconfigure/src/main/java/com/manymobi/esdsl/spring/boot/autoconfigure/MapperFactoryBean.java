52e3274c9e4463f44db51625613523f7ff126071
梁建军 <631583871@qq.com>
2021-08-19 18:59:02 +0800

    修复引入 spring-cloud-starter-sleuth 包后无法加载配置

--- esdsl-autoconfigure/src/main/java/com/manymobi/esdsl/spring/boot/autoconfigure/MapperFactoryBean.java
+++ esdsl-autoconfigure/src/main/java/com/manymobi/esdsl/spring/boot/autoconfigure/MapperFactoryBean.java
@@ -1,7 +1,10 @@
 package com.manymobi.esdsl.spring.boot.autoconfigure;
 
 import com.manymobi.esdsl.Esdsl;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * @author 梁建军
@@ -10,14 +13,17 @@ import org.springframework.beans.factory.FactoryBean;
  * @version 1.0
  * @since 1.0
  */
-public class MapperFactoryBean implements FactoryBean<Object> {
+public class MapperFactoryBean implements FactoryBean<Object>, ApplicationContextAware {
 
     private Class<Object> type;
 
-    private Esdsl esdsl;
+
+    private ApplicationContext applicationContext;
 
     @Override
     public Object getObject() throws Exception {
+
+        Esdsl esdsl = applicationContext.getBean(Esdsl.class);
         return esdsl.target(type);
     }
 
@@ -34,11 +40,8 @@ public class MapperFactoryBean implements FactoryBean<Object> {
         this.type = type;
     }
 
-    public Esdsl getEsdsl() {
-        return esdsl;
-    }
-
-    public void setEsdsl(Esdsl esdsl) {
-        this.esdsl = esdsl;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
